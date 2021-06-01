623f6157c72f076b5f320c9208cdeb17845e3665
Igor Lourenço <theigorlourenco@gmail.com>
2021-06-01 13:49:08 -0300

    fix: remove unused menu

--- src/components/common/cta-button.tsx
+++ src/components/common/cta-button.tsx
@@ -12,7 +12,7 @@ export const CTAButton = ({
 }: CTAButtonProps) => {
   return (
     <button
-      className={`rounded px-8 py-3 mt-4 text-md bg-${bgColor}-600 text-${fontColor} uppercase font-semibold hover:bg-${bgColor}-700 hover:shadow-lg focus:bg-${bgColor}-700 focus:shadow-lg focus:ring focus:ring-${bgColor}-400 focus:outline-none active:bg-${bgColor}-700 outline-none transition duration-400`}
+      className={`md:block md:w-full rounded px-8 py-3 mt-4 text-md bg-${bgColor}-600 text-${fontColor} uppercase font-semibold hover:bg-${bgColor}-700 hover:shadow-lg focus:bg-${bgColor}-700 focus:shadow-lg focus:ring focus:ring-${bgColor}-400 focus:outline-none active:bg-${bgColor}-700 outline-none transition duration-400`}
       {...props}
     >
       {props.children}
