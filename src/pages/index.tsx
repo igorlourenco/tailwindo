a741912306bcfaf894a04f25710a7e356e4a2870
Igor Lourenço <theigorlourenco@gmail.com>
2021-06-06 11:46:38 -0300

    feat: create section 1 - intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,39 @@ export default function Home() {
       </Head>
 
       <main>
-        <section>
-          <div>
-            <h1 className="text-brand-red">
-              O sucesso do seu time é o seu sucesso
-            </h1>
-            <h2>
-              Uma solução completa para cuidar do seu time e promover um
-              bem-estar real na sua empresa.
-            </h2>
-            <a>Vamos lá!</a>
+        <section className="bg-gradient-to-b from-white to-brand-papaya">
+          <div className="flex flex-row h-90">
+            <div className="w-1/2 z-20 flex items-center">
+              <div className="-mr-20 bg-brand-blue-light p-8 shadow-xl flex items-start flex-col gap-y-2">
+                <h1 className="text-white text-4xl font-bold">
+                  O sucesso do seu time é o seu sucesso
+                </h1>
+                <h2 className="text-white text-lg font-medium text-justify">
+                  Uma solução completa para cuidar do seu time e promover um
+                  bem-estar real na sua empresa.
+                </h2>
+                <a className="mt-4 bg-brand-yellow py-3 px-12 rounded-full font-semibold text-white text-lg">
+                  Vamos lá!
+                </a>
+              </div>
+            </div>
+            <div className="w-1/2 z-10 pl-20 pr-4">
+              <img src="/images/landing/team.svg" className="object-cover" />
+            </div>
           </div>
-          <div></div>
+          <div className="flex justify-center p-5">
+            <a
+              href="#employee-success"
+              className="animate-bounce font-semibold text-sm text-brand-blue-dark"
+            >
+              Conhecer melhor
+            </a>
+          </div>
+        </section>
+        <img src=" images/landing/base-section-1.svg"></img>
+
+        <section id="employee-success" className="-mt-32">
+          <p>IGRO</p>
         </section>
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
+//   mode: 'jit',
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -10,21 +10,24 @@ module.exports = {
 		  sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
 		  brand: '"Racing Sans One", cursive',
 		},
+		minWidth: {
+			'0': '0',
+			'1/4': '25%',
+			'1/2': '50%',
+			'3/4': '75%',
+			'full': '100%',
+		},
+		colors: {
+			'brand-blue-dark': '#086788',
+			'brand-blue-light': '#07A0C3',
+			'brand-yellow': '#f0C808',
+			'brand-papaya': '#FFF1D0',
+			'brand-red': '#DD1C1A'
+		},
+		backgroundImage: {
+			'base-section-1': '/images/landing/base-section-1.svg'
+		}
 	},
-	minWidth: {
-		'0': '0',
-		'1/4': '25%',
-		'1/2': '50%',
-		'3/4': '75%',
-		'full': '100%',
-	},
-	colors: {
-		'brand-blue-dark': '#086788',
-		'brand-blue-light': '#07A0C3',
-		'brand-yellow': '#f0C808',
-		'brand-papaya': '#FFF1D0',
-		'brand-red': '#DD1C1A'
-	}
   },
   variants: {
     extend: {
