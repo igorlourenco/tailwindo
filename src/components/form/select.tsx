80e88370c62aa1faf1c4d5bf447b5e6ce9828c73
Igor Lourenço <theigorlourenco@gmail.com>
2021-06-09 10:42:07 -0300

    style: change select background

--- src/components/form/select.tsx
+++ src/components/form/select.tsx
@@ -11,7 +11,7 @@ export default function Select({ options, ...props }: SelectProps) {
   return (
     <select
       required
-      className="appearance-none border border-gray-300 rounded-md w-full px-3 py-2 font-medium text-gray-600 outline-none focus:ring-1 active:ring-1 ring-blue-400"
+      className="appearance-none bg-transparent border border-gray-300 rounded-md w-full px-3 py-2 font-medium text-gray-600 outline-none focus:ring-1 active:ring-1 ring-blue-400"
       {...props}
     >
       <option value={null}> </option>
